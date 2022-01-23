8ecffe07c6d6a86e431e8e0a02c1fb91db28d929
gdwb3219 <gdwb3219@gmail.com>
2022-01-23 21:09:30 +0900

    add useEffect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import ReviewList from "./ReviewList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getReviews } from "../api";
 
 function App() {
@@ -15,10 +15,14 @@ function App() {
     setItems(nextItems)
   }
 
-  const handleLoadClick = async () => {
+  const handleLoad = async () => {
     const { reviews } = await getReviews();
     setItems(reviews);
   }
+  
+  useEffect(() => {
+    handleLoad()
+  }, [])
     
   return (
   <div>
@@ -27,7 +31,6 @@ function App() {
       <button onClick={handleBestClick}>별점순</button>
     </div>
     <ReviewList items={sortedItems} onDelete={handleDelete} />
-    <button onClick={handleLoadClick}>불러오기</button>
   </div>
   );
 }
