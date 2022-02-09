7303ad92072936c49414fb28a6ae5c25a900f4a4
gdwb3219 <gdwb3219@gmail.com>
2022-02-10 01:04:55 +0900

    How to use useRef

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -8,13 +8,22 @@ function FileInput({ name, value, onChange }) {
     onChange(name, nextValue)
   }
 
-  useEffect(() => {
-    if (inputRef.current) {
-      console.log(inputRef)
-    }
-  }, [])
+  const handleClearClick = () => {
+    const inputNode = inputRef.current
+    if (!inputNode) return
+    
+    inputNode.value = ''
+    onChange(name, null)
 
-  return <input type="file" onChange={handleChange} ref={inputRef}/>
+  }
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef}/>
+      {value && <button onClick={handleClearClick}>X</button>}
+    </div>
+  
+  )
 }
 
 export default FileInput
