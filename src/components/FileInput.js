9e2bbadfae2d6fc61aaa86496dcb9c1c67f957ce
gdwb3219 <gdwb3219@gmail.com>
2022-02-12 01:25:16 +0900

    Add Preview Image

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,6 +1,7 @@
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 function FileInput({ name, value, onChange }) {
+  const [preview, setPreview] = useState()
   const inputRef = useRef()
 
   const handleChange = (e) => {
@@ -9,7 +10,7 @@ function FileInput({ name, value, onChange }) {
     // console.log(e.target.value)
   }
 
-  const handleClearChange = () => {
+  const handleClearClick = () => {
     const inputNode = inputRef.current
     if (!inputNode) return
 
@@ -17,10 +18,18 @@ function FileInput({ name, value, onChange }) {
     onChange(name, null)
   }
 
+  useEffect(() => {
+    if (!value) return
+
+    const nextPreview = URL.createObjectURL(value)
+    setPreview(nextPreview)
+  }, [value])
+
   return (
     <div>
+      <img src={preview} alt="이미지 미리보기" />
       <input type="file" onChange={handleChange} ref={inputRef} />
-      {value && <button onClick={handleClearChange}>X</button>}
+      {value && <button onClick={handleClearClick}>X</button>}
     </div>
   )
 }
