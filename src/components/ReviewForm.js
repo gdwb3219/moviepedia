6cf02385dc0c602c1532d6aad3bd9d1305f3d6ad
gdwb3219 <gdwb3219@gmail.com>
2022-02-13 01:16:50 +0900

    Add Rating star

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,14 +1,18 @@
 import { useState } from "react"
+import { createReviews } from "../api"
 import FileInput from "./FileInput"
+import RatingInput from "./RatingInput"
 import './ReviewForm.css'
 
+const INITIAL_VALUES = {
+  title: '',
+  rating: 0,
+  content: '',
+  imgFile: null,
+}
+
 function ReviewForm() {
-  const [values, setValues] = useState({
-    title: '',
-    rating: 0,
-    content: '',
-    imgFile: null,
-  })
+  const [values, setValues] = useState(INITIAL_VALUES)
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -22,16 +26,22 @@ function ReviewForm() {
     handleChange(name, value)
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(values)
+    const formData = new FormData()
+    formData.append('title', values.title)
+    formData.append('rating', values.rating)
+    formData.append('content', values.content)
+    formData.append('imgFile', values.imgFile)
+    await createReviews(formData)
+    setValues(INITIAL_VALUES);
   }
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
       <FileInput name="imgFile" value={values.imgFile} onChange={handleChange} />
       <input name="title" value={values.title} onChange={handleInputChange} />
-      <input type="number" name="rating" value={values.rating} onChange={handleInputChange} />
+      <RatingInput name="rating" value={values.rating} onChange={handleChange} />
       <textarea name="content" value={values.content} onChange={handleInputChange} />
       <button type="submit">확인</button>
     </form>
