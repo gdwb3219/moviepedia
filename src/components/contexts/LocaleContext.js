0f0bd8e8f1749192f4e214b0edda49ad7adb92ab
gdwb3219 <gdwb3219@gmail.com>
2022-02-22 00:23:31 +0900

    Code Refactoring II

--- src/components/contexts/LocaleContext.js
+++ src/components/contexts/LocaleContext.js
@@ -5,12 +5,10 @@ const LocaleContext = createContext()
 export function LocaleProvider({ defaultValue = 'ko', children }) {
   const [locale, setLocale] = useState(defaultValue)
   return (
-    <LocaleContext.Provider value={
-      {
+    <LocaleContext.Provider value={{
         locale,
         setLocale,
-      }
-    }>
+      }}>
     {children}
     </LocaleContext.Provider>
   )
