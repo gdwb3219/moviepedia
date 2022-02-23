7cfe059f13dcc07d683c7457731026a7f9356322
gdwb3219 <gdwb3219@gmail.com>
2022-02-24 01:25:26 +0900

    delete unuseless comment

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -7,7 +7,6 @@ function FileInput({ name, value, initialPreview, onChange }) {
   const handleChange = (e) => {
     const nextValue = e.target.files[0]
     onChange(name, nextValue)
-    // console.log(e.target.value)
   }
 
   const handleClearClick = () => {
